d337fffd03cf3e76fcea47b8aca28cd9182b4dc5
Rez <reza.amini.majd1379@gmail.com>
2020-01-06 18:24:37 +0330

    first

--- backend/settings.py
+++ backend/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post',
+    'apps.post',
     'corsheaders',
     'rest_framework',
 ]
