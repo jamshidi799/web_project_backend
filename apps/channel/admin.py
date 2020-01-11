2895bf95f261a8d6c06093e79d770bb02bdf0471
Mohammad <mohammad.ce79@gmail.com>
2020-01-12 00:12:39 +0330

    add models to admin

--- apps/channel/admin.py
+++ apps/channel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Channel
 
-# Register your models here.
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'about')
+
+admin.site.register(Channel, ChannelAdmin)
--- apps/channel/models.py
+++ apps/channel/models.py
@@ -5,6 +5,7 @@ class Channel(models.Model):
     owner = models.ForeignKey('auth.User',
                               on_delete=models.CASCADE,
                               related_name='channels')
+    name = models.CharField(max_length=20, default='')
     about = models.CharField(max_length=300)
     image = models.ImageField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
